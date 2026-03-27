c20e3220b65d3ae965ff71c306011b833d05f265
MERT6776 <mertafacan431@gmail.com>
2026-03-27 08:58:18 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Sayfa ayarları
+st.set_page_config(page_title="KSK Puantaj", layout="centered")
+
+# Geçici Ana Ekran
+st.title("🚧 KSK PUANTAJ SİSTEMİ 🚧")
+st.success("Motor çalıştı, altyapı kuruldu! KSK şantiyesi için Excel bağlantısı ve arayüz bekleniyor...")
